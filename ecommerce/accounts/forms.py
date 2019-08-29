0ddc43e959aa90efa4084978efe5bf8a1fc31222
Ubuntu <ubuntu@ip-172-31-92-234.ec2.internal>
2019-08-29 09:36:41 +0000

    Password Validation complete on registration page

--- ecommerce/accounts/forms.py
+++ ecommerce/accounts/forms.py
@@ -27,7 +27,7 @@ class UserRegistrationForm(UserCreationForm):
         model= User
         fields = ['email', 'username', 'password1', 'password2']
     
-# Form Validation
+# Email Validation 
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -36,4 +36,14 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError(u'Email address must be unique')
         return email
         
+# Password Validation        
         
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if not password1 or password2:
+            raise forms.ValidationError("Please confirm your password")
+        if password1 != password2:
+            raise ValidationError("Passwords must match!")
+        return password2    
