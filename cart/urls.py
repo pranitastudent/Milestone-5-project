c213b8eb4f1f9b3c355f2cfd7d3d06e6bf47d6ae
Ubuntu <ubuntu@ip-172-31-92-234.ec2.internal>
2019-10-30 11:40:58 +0000

    Cart Views added - all , add, adjust

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from .views import view_cart, add_to_cart, adjust_cart
 
+# Code adapted from Code Institute Ecommerce Project
+
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
     url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart')
     ]
+    
+    
+    
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+# View Everything in Cart
+
+def view_cart(request):
+    
+    return render(request, "cart.html")
+    
+    
+# Add To Cart
+
+def add_to_cart(request, id):
+   
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+    
+# Adjust Cart- Quantity against Price
+
+
+def adjust_cart(request, id):
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
