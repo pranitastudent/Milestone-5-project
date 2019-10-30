7aaa0d93d4be1acdd0466aab990aa8fd97108390
Ubuntu <ubuntu@ip-172-31-92-234.ec2.internal>
2019-10-30 11:07:21 +0000

    Product urls added to eccommerce project urls

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,9 +132,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,15 +6,20 @@ from django.views.static import serve
 from django.contrib import admin
 from .settings import MEDIA_ROOT
 from accounts.views import index
-from accounts import urls as accounts_urls
+from accounts import urls as urls_accounts
+from products import urls as urls_products
+from products.views import all_products
+from django.views import static
+
 
 
 # Url Patterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
-    url(r'^accounts/', include(accounts_urls)),
+    url ('r^$', all_products, name= "index"),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root':MEDIA_ROOT}),
   
     
