5a957c1fa3f7f859273298e9f555dcd52a92ae87
Ubuntu <ubuntu@ip-172-31-89-157.ec2.internal>
2019-10-31 14:14:17 +0000

    Created checkout.html - added html and Stripe publishable key

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -11,6 +11,7 @@ from products.views import all_products
 from cart import urls as urls_cart
 from django.views import static
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 
 
 
@@ -24,6 +25,7 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root':MEDIA_ROOT}),
+    url(r'^checkout/', include(urls_checkout)),
   
     
 ]
