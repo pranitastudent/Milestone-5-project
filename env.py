afdf7664137928d8bb34669bd31a501759ace61c
Ubuntu <ubuntu@ip-172-31-92-234.ec2.internal>
2019-10-31 10:34:08 +0000

    Migrating to new Cloud 9 Environment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["EMAIL_ADDRESS"] = 'pranitacoder12@gmail.com'
+os.environ["EMAIL_PASSWORD"] = 'Maturase12'
+os.environ["SECRET_KEY"]='1m*jupi3w40z*+1-k&b$v%_-22x!+tl-w%4yz!_3-=$@$mz^bc'
--- products/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Product
-
-
-class Product(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields['size']
