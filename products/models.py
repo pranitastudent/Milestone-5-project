aa3273ec242bdce44b48d9a01189ae7566582b9d
Ubuntu <ubuntu@ip-172-31-89-157.ec2.internal>
2019-11-04 16:02:44 +0000

    Added Size filters

--- products/models.py
+++ products/models.py
@@ -6,6 +6,14 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    Size_CHOICES = (
+    ('Small', 'Small'),
+    ('Medium', 'Medium'),
+    ('Large', 'Large'),
+    ('XLarge', 'XLarge'),
+    ('XXLarge', 'XXLarge')
+    )
+    rating = models.IntegerField(choices=Size_CHOICES, default='Medium')
 
     def __str__(self):
         return self.name
