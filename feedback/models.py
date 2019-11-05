aace0aafb8c6ae183351714427c9d036708a663a
Ubuntu <ubuntu@ip-172-31-89-157.ec2.internal>
2019-11-05 14:26:08 +0000

    Feedback model works without default problem

--- feedback/models.py
+++ feedback/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Feedback(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    title = models.CharField(max_length=100)
+    feedback = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    Rating_CHOICES = (
+    ('Poor', 'Poor'),
+    ('Average', 'Average'),
+    ('Good', 'Good'),
+    ('Very Good', 'Very Good'),
+    ('Excellent', 'Excellent')
+    )
+    rating = models.IntegerField(choices=Rating_CHOICES, default=None)
+    views = models.IntegerField(default=0)
+    
